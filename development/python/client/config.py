d019eec3c8f9965ed84a2ba08f212de5058b15de
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2021-06-08 11:17:56 +0530

    cQube workflow

---
+++ development/python/client/config.py
@@ -0,0 +1,3 @@
+access_token=""   #API Key
+file_path=""      #'/home/ubuntu/cqube_emission/'
+emission_url=""   #http://ip:5000/
---
+++ development/python/server.py
@@ -0,0 +1,6 @@
+from views import *
+from config import *
+from views import app as application
+
+if __name__=="__main__":
+    application.run(host='0.0.0.0')
