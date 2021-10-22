bf9230ca166d1c975d7fd579a6804c16abef8a7e
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2021-10-22 13:17:08 +0530

    added update tags in upgrade.sh update_ui.sh

--- workflow_deploy/education_usecase_theme2/update_ui.sh
+++ workflow_deploy/education_usecase_theme2/update_ui.sh
@@ -3,7 +3,7 @@
 base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
 mode_of_installation=$(awk ''/^mode_of_installation:' /{ if ($2 !~ /#.*/) {print $2}}' $base_dir/cqube/conf/base_config.yml)
 if [[ $mode_of_installation == "localhost" ]]; then
-ansible-playbook ../ansible/update_ui.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
+ansible-playbook ../ansible/update_ui.yml --tags "update" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
                                                              --extra-vars "@config.yml" \
                                                                                          --extra-vars "@memory_config.yml" \
                                                              --extra-vars "@.version" \
@@ -13,7 +13,7 @@ ansible-playbook ../ansible/update_ui.yml --tags "install" --extra-vars "@$base_
                                                              --extra-vars "usecase_name=education_usecase_theme2" \
                                                              --extra-vars "protocol=http"
 else
-ansible-playbook ../ansible/update_ui.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
+ansible-playbook ../ansible/update_ui.yml --tags "update" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
                                                              --extra-vars "@config.yml" \
                                                                                          --extra-vars "@memory_config.yml" \
                                                              --extra-vars "@.version" \
