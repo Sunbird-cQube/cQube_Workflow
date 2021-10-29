c76a7b4ebfee5ab15fd2e820bde209beed81095f
krishnakr6 <krishna.raj@tibilsolutions.com>
2021-10-29 11:46:15 +0530

    added diksha_transformer_custom

--- development/datasource/python/nifi_start_pg.py
+++ development/datasource/python/nifi_start_pg.py
@@ -1,14 +1,15 @@
 from deploy_nifi import get_nifi_root_pg, get_processor_group_info, rq, prop, logging, sys
 
 
-def start_processor_group(processor_group_name):
+def start_processor_group(processor_group_name,state):
+    header = {"Content-Type": "application/json"}
     pg_source = get_processor_group_info(processor_group_name)
     start_body = {"id": pg_source['component']['id'],
-                  "state": "RUNNING", "disconnectedNodeAcknowledged": False}
+                  "state": state, "disconnectedNodeAcknowledged": False}
     start_response = rq.put(f"{prop.NIFI_IP}:{prop.NIFI_PORT}/nifi-api/flow/process-groups/{pg_source['component']['id']}",
                             json=start_body, headers=header)
     if start_response.status_code == 200:
-        logging.info(f"Successfuly scheduled {pg_source['component']['name']}")
+        logging.info(f"Successfully {state} {pg_source['component']['name']} Processor Group.")
         return True
     else:
         return start_response.text
@@ -23,7 +24,10 @@ if __name__ == "__main__":
     """
     header = {"Content-Type": "application/json"}
     processor_group_name = sys.argv[1]
+    state = sys.argv[2]
     
-    # start telemetry processor group = cqube_telemetry_transformer
-    start_processor_group(processor_group_name)
+    
+    # enable/disable/start/stop the processor group 
+    start_processor_group(processor_group_name,state)
+
 
