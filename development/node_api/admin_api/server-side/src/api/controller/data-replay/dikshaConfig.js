420b7e3c31552bc4406a81b32f1afc68cc6d0988
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2021-11-02 15:40:42 +0530

    Removed unwanted commented code on Dikshconfig

--- development/node_api/admin_api/server-side/src/api/controller/data-replay/dikshaConfig.js
+++ development/node_api/admin_api/server-side/src/api/controller/data-replay/dikshaConfig.js
@@ -9,10 +9,7 @@ router.post('/', async (req, res) => {
         let fromDate = req.body.fromDate;
         let toDate = req.body.toDate;
         let selectedHour = req.body.hourSelected;
-        // shell.exec(type == 'Default' ?
-        //     `sudo ${process.env.BASE_DIR}/cqube/emission_app/flaskenv/bin/python ${baseDir}/cqube/emission_app/python/update_processor_property.py diksha_transformer` :
-        //     `sudo ${process.env.BASE_DIR}/cqube/emission_app/flaskenv/bin/python ${baseDir}/cqube/emission_app/python/update_processor_property.py diksha_transformer_custom  ${fromDate} ${toDate} ${selectedHour}`,
-
+        
             shell.exec(`sudo ${process.env.BASE_DIR}/cqube/emission_app/flaskenv/bin/python ${baseDir}/cqube/emission_app/python/update_processor_property.py diksha_transformer_custom  ${fromDate} ${toDate} ${selectedHour}`,
            
             function (code, stderr, stdout) {
