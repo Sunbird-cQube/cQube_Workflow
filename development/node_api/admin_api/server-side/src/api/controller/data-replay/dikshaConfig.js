7b9cedf2a419ef1f77d48b91e30d9e7ed4ce4f0d
dheeraj8008baba <dheerajtiwari546@gmail.com>
2021-11-09 15:12:11 +0530

    Diksha-config issue fixed

--- development/node_api/admin_api/server-side/src/api/controller/data-replay/dikshaConfig.js
+++ development/node_api/admin_api/server-side/src/api/controller/data-replay/dikshaConfig.js
@@ -9,19 +9,12 @@ router.post('/', async (req, res) => {
         let fromDate = req.body.fromDate;
         let toDate = req.body.toDate;
         let selectedHour = req.body.hourSelected;
-        
-            shell.exec(`sudo ${process.env.BASE_DIR}/cqube/emission_app/flaskenv/bin/python ${baseDir}/cqube/emission_app/python/update_processor_property.py diksha_transformer_custom  ${fromDate} ${toDate} ${selectedHour}`,
-           
-            function (code, stderr, stdout) {
-                if (stderr) {
-                    logger.error('Program stderr:', stderr);
-                    res.status(403).send({ errMsg: "Something went wrong" });
-                } else {
-                    logger.info('--- diksha configuration api response sent---');
-                    res.status(200).send({ msg: `Diksha ETB Dates Configured Successfully.` });
-                }
-            });
 
+        if (shell.exec(`sudo ${process.env.BASE_DIR}/cqube/emission_app/flaskenv/bin/python ${baseDir}/cqube/emission_app/python/update_processor_property.py diksha_transformer_custom  ${fromDate} ${toDate} ${selectedHour}`).code == 1) {
+            logger.error("Something went wrong");
+            res.status(406).send({ errMsg: "Something went wrong" });
+        }
+        return false;
     } catch (e) {
         logger.error('--- Internal Server Error ---');
         res.status(500).send({ errMsg: "Internal Server Error" });
