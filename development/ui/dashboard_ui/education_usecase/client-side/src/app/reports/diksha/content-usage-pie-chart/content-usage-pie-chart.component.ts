edec73b7927be5bf80353ace5700f3f8a0ff124a
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2021-12-01 12:02:04 +0530

    Diskha State and State with District pie chart with demo data

--- development/ui/dashboard_ui/education_usecase/client-side/src/app/reports/diksha/content-usage-pie-chart/content-usage-pie-chart.component.ts
+++ development/ui/dashboard_ui/education_usecase/client-side/src/app/reports/diksha/content-usage-pie-chart/content-usage-pie-chart.component.ts
@@ -5,6 +5,11 @@ import { AppServiceComponent } from 'src/app/app.service';
 import { ContentUsagePieService } from 'src/app/services/content-usage-pie.service';
 import { MapService } from 'src/app/services/map-services/maps.service';
 
+import addMore from "highcharts/highcharts-more";
+import HC_exportData from 'highcharts/modules/export-data';
+HC_exportData(Highcharts);
+addMore(Highcharts)
+
 @Component({
   selector: 'app-content-usage-pie-chart',
   templateUrl: './content-usage-pie-chart.component.html',
@@ -34,6 +39,9 @@ export class ContentUsagePieChartComponent implements OnInit {
     this.height = window.innerHeight;
   }
 
+  public stateDropDown = [{ key: 'State Only', name: 'State Only' }, { key: 'State with Districts', name: 'State with Districts' }]
+  selectedDrop = 'State with Districts'
+  
   ngOnInit(): void {
     this.commonService.errMsg();
     this.changeDetection.detectChanges();
@@ -41,7 +49,8 @@ export class ContentUsagePieChartComponent implements OnInit {
     document.getElementById("accessProgressCard").style.display = "none";
     document.getElementById("backBtn") ? document.getElementById("backBtn").style.display = "none" : "";
     this.getStateData();
-    this.getDistData();
+    // this.getDistData();
+    // this.createDistPiechart();
     
   }
  
@@ -57,6 +66,7 @@ try {
    this.stateData = this.restructurePieChartData(this.pieData)
    this.createPieChart(this.stateData);
    this.getDistMeta();
+   this.getDistData()
    this.commonService.loaderAndErr(this.stateData);
    })
 } catch (e) {
@@ -71,27 +81,47 @@ try {
   this.dist =false;
   this.skul = true;
   this.getStateData();
+  
 }
   
 
    restructurePieChartData(pieData){
+    
       let data:any = []
     pieData.forEach( item => {
        data.push({
             name: item.collection_type,
             y: Number((Math.round(item.total_content_plays_percent * 100) / 100).toFixed(2))
         })
-      }) 
+      })
+      console.log('state', data) 
       return data
+      
    }
 
+   disWisePieChart : any = []
 
  /// distData
  getDistData(){
+  // let dataPie = []
      try {
         this.service.dikshaPieDist().subscribe(res => {
             this.distData = res['data'].data;
-           console.log('dist',this.distData)
+            // this.distToDropDown.forEach(item => {
+            //   this.distWisePieData = this.distData[Number(item['district_id'])]
+            
+            //   dataPie.push({
+            //    name: this.distData[Number(item['district_id'])]
+            //  })
+            //  console.log('pieData',this.distWisePieData)
+            let obj1: any = []; let obj2 = {}
+            Object.keys(this.distData).forEach( keys => {
+              obj1.push(
+                this.distData[keys])
+            })
+            console.log('obj',obj1)
+            // Object.keys(this.distData)
+            this.createDistPiechart(obj1)
             }) 
      } catch (error) {
           console.log(error)
@@ -103,19 +133,31 @@ try {
      public distToDropDown
   /// distMeta
   getDistMeta(){
-    try {
+       try {
        this.service.diskshaPieMeta().subscribe(res => {
            this.distMetaData = res['data'];
-           console.log('metaData', this.distMetaData)
+          
           this.distToDropDown = this.distMetaData.Districts.map( (dist:any) =>{
               return dist
           })
-          console.log('drop',this.distToDropDown)
+        
            }) 
     } catch (error) {
         //  console.log(error)
     }
   
+}
+
+ public distToggle = true
+
+onStateDropSelected(data){
+  this.selectedDrop = data
+  if(this.selectedDrop === 'State with Districts'){
+    this.distToggle = true
+  }else{
+    this.distToggle = false
+  }
+  this.getStateData();
 }
 
  public selectedDist;
@@ -130,7 +172,7 @@ try {
      this.dist = true;
      this.skul = false;
      this.distName = '';
-     console.log('distName', data)
+    
     try {
       this.selectedDist = data;
       this.distWiseData = this.distData[this.selectedDist]
@@ -140,11 +182,331 @@ try {
     } catch (error) {
       this.distWiseData = [];
       this.commonService.loaderAndErr(this.distWiseData);
-      console.log(error)
+    
     }
   }
 
+  distPieChart 
+  disttoLoop = []
+  allDistData
+  distWisePieData
+ 
+  createDistPiechart(data ){
+     console.log('dataforie', data)
+    // var allDistData
+    // let distWiseData
+    let pieData = data
+    let Distdata:any = []
+    // let data1:any = []
+    //     data.forEach((item, i) =>{
+    //       data1.push({
+    //         name: item.data[i].collection_type,
+    //         y: Number((Math.round(item.data[i].total_content_plays_percent * 100) / 100).toFixed(2))
+    //     })
+              // item.forEach( item1 =>{
+              //   data1.push({
+              //     name: item1.collection_type,
+              //     y: Number((Math.round(item1.total_content_plays_percent * 100) / 100).toFixed(2))
+              // })
+              // })
+        // })
+    // console.log('data1', data1)
+
+    this.service.diskshaPieMeta().subscribe(res => {
+      this.distPieChart = res['data'];
+      // console.log('distPieChart', this.distPieChart )
+     this.disttoLoop = this.distPieChart.Districts.map( (dist:any) =>{
+         return dist
+     })
+    
+     console.log('dist', this.disttoLoop)
+     ////data///////
+    
+     let createdDiv
+      
+     let containerID = []
+     var Piedata = [{
+       distName: "Ahmedabad",
+      data1: 12,
+      data2: 25,
+      data3: 40
+    }, {
+      distName: "Aravalli",
+      data1: 67,
+      data2: 11,
+      data5: 56
+    },
+    {
+      distName: "Navsari", 
+      data1: 67,
+      data2: 11,
+      data5: 56
+    },
+    { 
+      distName: "Junagadh",
+      data1: 67,
+      data2: 11,
+      data5: 56
+    },
+    {
+      distName: "Panch Mahals",
+      data1: 67,
+      data2: 11,
+      data5: 56
+    },{
+      distName: "Bharuch",
+      data1: 67,
+      data2: 11,
+      data5: 56
+    },{
+      distName: "Anand",
+     data1: 12,
+     data2: 25,
+     data3: 40
+   }, {
+     distName: "Surat",
+     data1: 67,
+     data2: 11,
+     data5: 56
+   },
+   {
+     distName: "Kheda", 
+     data1: 67,
+     data2: 11,
+     data5: 56
+   },
+   { 
+     distName: "Kachchh",
+     data1: 67,
+     data2: 11,
+     data5: 56
+   },
+   {
+     distName: "Chhotaudepur",
+     data1: 67,
+     data2: 11,
+     data5: 56
+   },{
+     distName: "The Dangs",
+     data1: 67,
+     data2: 11,
+     data5: 56
+   },
+   {
+    distName: "Sabar Kantha",
+   data1: 12,
+   data2: 25,
+   data3: 40
+ }, {
+   distName: "Gandhinagar",
+   data1: 67,
+   data2: 11,
+   data5: 56
+ },
+ {
+   distName: 'Amreli', 
+   data1: 67,
+   data2: 11,
+   data5: 56
+ },
+ { 
+   distName: 'Rajkot',
+   data1: 67,
+   data2: 11,
+   data5: 56
+ },
+ {
+   distName: 'Mahisagar',
+   data1: 67,
+   data2: 11,
+   data5: 56
+ },{
+   distName: 'Vadodara',
+   data1: 67,
+   data2: 11,
+   data5: 56
+ },{
+   distName: 'Bhavnagar',
+  data1: 12,
+  data2: 25,
+  data3: 40
+}, {
+  distName: "Narmada",
+  data1: 67,
+  data2: 11,
+  data5: 56
+},
+{
+  distName: 'Mahesana', 
+  data1: 67,
+  data2: 11,
+  data5: 56
+},
+{ 
+  distName: 'Tapi',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},
+{
+  distName: 'Surendranagar',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},{
+  distName: 'Devbhoomi Dwarka',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},{
+  distName: 'Valsad', 
+  data1: 67,
+  data2: 11,
+  data5: 56
+},
+{ 
+  distName: 'Jamnagar',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},
+{
+  distName: 'Patan',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},{
+  distName: 'Banaskantha',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},{ 
+  distName:  'Botad',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},
+{
+  distName: 'Morbi',
+  data1: 67,
+  data2: 11,
+  data5: 56
+},{
+  distName: 'Gir Somnath',
+  data1: 67,
+  data2: 11,
+  data5: 56
+}];
+
+    // var Piedata = [{
+    //   "name": "cakes",
+    //   "data": [
+    //     {
+    //      name: "us",
+    //      y: 149
+    //     },
+    //     {
+    //       name: "us",
+    //       y: 149
+    //      },
+    //      {
+    //       name: "us",
+    //       y: 149
+    //      },
+    //      {
+    //       name: "us",
+    //       y: 149045
+    //      }
+    //   ],
+    // }, {
+    //   "name": "pie",
+    //   "data": [
+    //     {
+    //      name: "us",
+    //      y: 149
+    //     },
+    //     {
+    //       name: "us",
+    //       y: 149
+    //      },
+    //      {
+    //       name: "us",
+    //       y: 149
+    //      },
+    //      {
+    //       name: "us",
+    //       y: 149045
+    //      }
+    //   ]
+    // }]
+    
+ 
+     const  mainContainer = document.getElementById('container1');
+    // console.log('distToLoop', this.disttoLoop)
+    Piedata.forEach(function(el:any,i) {
+      var chartData = [el.data1, el.data2, el.data3];
+      var distName = el.distName
+      console.log('ddd', distName)
+      // var chartData = [el.data[i].y];
+     createdDiv = document.createElement('div');
+      createdDiv.style.display = 'inline-block';
+      createdDiv.style.width = '300px';
+      createdDiv.style.height = "300px";
+      createdDiv.id = `text${i}`
+      // createdDiv.style.backgroundColor = 'blue';
+      
+     
+      
+      mainContainer.appendChild(createdDiv); 
+      // Object.keys(dataEl).forEach( item=> {
+      //   console.log('item',item)
+      // })
+   
+     
+     
+     
+      // let pieData:any =  this.data[Number(dataEl['district_id'])]
+      Highcharts.chart(createdDiv, {
+        chart:{
+          plotBackgroundColor: 'transparent',
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie',
+          backgroundColor: 'transparent',
+
+        },
+        title:{
+          text:  `${distName}`,
+        },
+        colors: ['#50B432', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+        credits: {
+          enabled: false
+        },
+        plotOptions: {
+          pie: {
+            size: 130,
+          }
+        },
+        series: [{
+          type: 'pie',
+          colorByPoint: true,
+          data: chartData
+        }]
+
+        
+      });
+     
+    })     
+        });
+      // }) 
+    }
+
+   
+
   createPieChart(data){
+
+      ////////
+
     this.chartOptions = { 
       chart: {
         plotBackgroundColor: 'transparent',
@@ -155,7 +517,11 @@ try {
     },
     colors: ['#50B432', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
     title: {
-        text: 'Total'
+        text: '',
+        align: 'left',
+        style: {
+          fontWeight: 'bold',
+        }  
     },
     tooltip: {
         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
@@ -176,17 +542,18 @@ try {
     },
     legend: {
       layout:  'vertical',
-      align: 'right',
-      verticalAlign: 'middle',
+      align: 'left',
+      // horizontalAlign: 'middle',
       borderWidth: 0,
-      width: '40%',
-      itemWidth: '30%',
-      itemMarginTop: 5,
-      itemMarginBottom: 5,
+      // width: '70%',
+      // itemWidth: '30%',
+      // itemMarginTop: 10,
+      itemMarginBottom: 7,
       style: {
         // color: 'blue',
         // fontWeight: 'bold',
-        fontSize: '0.8rem'
+        fontSize: '0.8rem',
+        lineHeight: 2
     }
     },
     plotOptions: {
@@ -235,6 +602,10 @@ try {
     };
     this.Highcharts.chart("container", this.chartOptions);
   }
+
+  
+
+ 
 
   
 }
