846c751811e924ca188de68d4c99a005998c46fa
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2021-12-08 15:50:05 +0530

    Multi bar chart with expected enrolled bar

--- development/node_api/dashboard_api/server-side/src/api/controller/diksha/tpd-enroll-completion/distWise.js
+++ development/node_api/dashboard_api/server-side/src/api/controller/diksha/tpd-enroll-completion/distWise.js
@@ -20,7 +20,14 @@ router.post('/allDistData', auth.authController, async (req, res) => {
         })
         
         chartData['data'] = jsonData.map(a => {
-            return { enrollment: a.total_enrolled, completion: a.total_completed, percent_teachers: a.percentage_teachers, percent_completion: a.percentage_completion, expected_enrolled: a.expected_total_enrolled, expected_enrolled_percentage:a.expected_enrollment_percentage,}
+            return { enrollment: a.total_enrolled, 
+                completion: a.total_completed, 
+                 percent_completion: a.percentage_completion, 
+                 expected_enrolled: a.expected_total_enrolled, 
+                 enrolled_percentage:a.total_enrolled_percentage,
+                 certificate_value: a.certificate_count,
+                 certificate_per: a.certificate_percentage
+                }
         })
         logger.info('--- diksha chart allData api response sent ---');
         res.send({ chartData, downloadData: jsonData, footer });
