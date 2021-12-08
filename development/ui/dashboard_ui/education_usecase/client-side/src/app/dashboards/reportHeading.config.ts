c5219d67406f6491bd8cffb08b4ade9fb3e68367
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2021-12-08 18:56:21 +0530

    Report Name configuration

---
+++ development/ui/dashboard_ui/education_usecase/client-side/src/app/dashboards/reportHeading.config.ts
@@ -0,0 +1,62 @@
+import * as config from '../../assets/config.json';
+import { environment } from '../../environments/environment';
+
+let state = config.default[`${environment.stateName}`].name;
+export const dashboardReportHeadings = {
+ 
+  // ----School Infrastructure reports -----
+   InfrastructureAccessbyLocation : "Infrastructure Access by Location",
+   CompositeReport : "Composite Report",
+   UDISEReport : "UDISEReport",
+
+   // ---- student Perfomance reports ----
+   SemesterAssesmentTest:"Semester Assesment Test",
+   SemesterAssesmentTestHeatchart: "Semester Assesment Test Heat-chart",
+   PeriodicAssesmentTest: "Periodic Assesment Test",
+   PeriodicAssesmentTestHeatchart : "Periodic Assesment Test Heat-chart",
+   PeriodicAssesmentTestLOTable : "Periodic Assesment Test LO-Table",
+
+    // --- student Attendance reports ---
+   StudentAttendance: "Student Attendance",
+   TeacherAttendance : "Teacher Attendance",
+
+   // ----  Teacher Profesional Development reports ------
+   UsagebyCourse  : "Usage by Course",
+   UsageByCourseContent : "Usage By Course Content",
+   TPDCourseProgress  : "TPD Course Progress",
+   UserProgress:   "User Progress", 
+   tpdGpsOfLearning: "GPS of learning- TPD",
+   ContentPreference : "Content Preference",
+   UserEngagement: "User Engagement",
+
+    // ---- ETB reports --------
+   UsagebyUserProfile   : "Usage by User Profile",
+   UsagebyTextbook    : "Usage by Textbook ",
+   UsageByTextbookContent : "Usage By Textbook Content",
+   etbGpsOfLearning: "GPS of learning- ETB",
+   Usagepercapita :"Usage per capita",
+   heartbeatOfTheNationLearning:"  Heartbeat of the Nation Learning",
+   UserOnboarding: "User Onboarding",
+   
+   // ------ crc visits reports ---------
+   CRCReport : "CRC Report",
+   
+   // ------ composite reports across metrics----
+   CompositeReportAcrossMetrics : "Composite Report Across Metrics",
+
+  // ------- progress card reports ---
+   ProgressReport : "Progress Report",
+ 
+   // ------Exception reports -----
+   SemesterAssesmentTestException : "Semester Assesment Test Exception",
+   PeriodicAssesmentTestException: "Periodic Assesment Test Exception",
+   DownloadExceptionList : "Download Exception List",
+   StudentAttendanceReportException : "Student Attendance Report Exception",
+   TeacherAttendanceReportException: "Teacher Attendance Report Exception",
+
+  // --- telemetry reports -------
+  Telemetry: "Telemetry"
+ 
+ 
+
+}
