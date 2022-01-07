fd535696443f0e20dc9b8d881f136b7859e98ffa
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-01-07 11:09:44 +0530

    changed education_usecase_theme2 folder name to test_usecase

--- workflow_deploy/test_usecase/install.sh
+++ workflow_deploy/test_usecase/install.sh
@@ -12,52 +12,66 @@ fi
 INS_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$INS_DIR" ]]; then INS_DIR="$PWD"; fi
 
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible-2.9 -y
 sudo apt update -y
-chmod u+x upgradation_validate.sh
+sudo apt install python -y
+sudo apt-get install python-apt -y
+sudo apt-get install python3-pip -y
+chmod u+x validate.sh
+sudo apt install unzip -y
 
 if [[ ! -f config.yml ]]; then
     tput setaf 1; echo "ERROR: config.yml is not available. Please copy config.yml.template as config.yml and fill all the details."; tput sgr0
     exit;
 fi
 
-. "upgradation_validate.sh"
-. "datasource_validation.sh" 
+. "validate.sh"
+. "datasource_validation.sh"
+
+sudo apt install ansible -y
 
 if [ -e /etc/ansible/ansible.cfg ]; then
 	sudo sed -i 's/^#log_path/log_path/g' /etc/ansible/ansible.cfg
 fi
 
-base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
-ansible-playbook ../ansible/create_base.yml --tags "update" --extra-vars "@config.yml" --extra-vars "@$base_dir/cqube/conf/base_config.yml" 
-. "$INS_DIR/validation_scripts/backup_postgres.sh" config.yml
+echo '127.0.0.0' >> /etc/ansible/hosts
 
+if [ ! $? = 0 ]; then
+tput setaf 1; echo "Error there is a problem installing Ansible"; tput sgr0
+exit
+fi
+   
+. "$INS_DIR/validation_scripts/validate_static_datasource.sh" config.yml 
 base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
+
 mode_of_installation=$(awk ''/^mode_of_installation:' /{ if ($2 !~ /#.*/) {print $2}}' $base_dir/cqube/conf/base_config.yml)
 if [[ $mode_of_installation == "localhost" ]]; then
-ansible-playbook ../ansible/upgrade.yml --tags "update" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
+ansible-playbook ../ansible/install.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
                                                          --extra-vars "@config.yml" \
-                                                                                     --extra-vars "@memory_config.yml" \
+							                             --extra-vars "@memory_config.yml" \
                                                          --extra-vars "@.version" \
                                                          --extra-vars "@$base_dir/cqube/conf/aws_s3_config.yml" \
 														 --extra-vars "@$base_dir/cqube/conf/azure_container_config.yml" \
                                                          --extra-vars "@$base_dir/cqube/conf/local_storage_config.yml" \
-                                                                                     --extra-vars "@datasource_config.yml" \
-                                                         --extra-vars "usecase_name=education_usecase_theme2" \
+							                             --extra-vars "@datasource_config.yml" \
+                                                         --extra-vars "usecase_name=test_usecase" \
                                                          --extra-vars "protocol=http"
 else
-ansible-playbook ../ansible/upgrade.yml --tags "update" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
+ansible-playbook ../ansible/install.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
                                                          --extra-vars "@config.yml" \
-                                                                                     --extra-vars "@memory_config.yml" \
+							                             --extra-vars "@memory_config.yml" \
                                                          --extra-vars "@.version" \
                                                          --extra-vars "@$base_dir/cqube/conf/aws_s3_config.yml" \
 														 --extra-vars "@$base_dir/cqube/conf/azure_container_config.yml" \
                                                          --extra-vars "@$base_dir/cqube/conf/local_storage_config.yml" \
-                                                                                     --extra-vars "@datasource_config.yml" \
-                                                         --extra-vars "usecase_name=education_usecase_theme2"
+							                             --extra-vars "@datasource_config.yml" \
+                                                         --extra-vars "usecase_name=test_usecase"
 fi
 if [ $? = 0 ]; then
-. "update_ui.sh"
+. "install_ui.sh"
     if [ $? = 0 ]; then
-       echo "cQube Workflow upgraded successfully!!"
+       echo "cQube Workflow installed successfully!!"
     fi
 fi
+
