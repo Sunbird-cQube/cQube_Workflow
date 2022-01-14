433c4a5f1e02db5c4c00fb9ce579214b2566ebcc
krishnakr6 <krishna.raj@tibilsolutions.com>
2022-01-14 10:41:45 +0530

    updated python file - diksha enrolment

--- development/datasource/python/diksha_enrolment_file_split.py
+++ development/datasource/python/diksha_enrolment_file_split.py
@@ -8,17 +8,14 @@ import boto3
 import sys
 from update_batch_id import update_parameter_ctx
 
-
 def creat_csv_file(list_of_items, filename):
-    global local_path
     f_name = filename.lower()
     f = f_name.split(' ', 2)[2]
     f2 = f.replace('-', ' ')
     removeSpecialChars = f2.replace(' ', '_')
     filename_d = 'diksha_' + removeSpecialChars + '.csv'
-    local_path = 'new_diksha_directory/'
-    if not os.path.exists(local_path):
-        os.mkdir(local_path)
+
+
 
     with open(filename_d, 'w') as file:
         csvwriter = csv.writer(file, delimiter='|')
@@ -44,6 +41,8 @@ def creat_csv_file(list_of_items, filename):
 
 
 
+
+
 def emission_folder(local_path,destination_path,storage_type):
     if storage_type == 'local':
         src_files = os.listdir(local_path)
@@ -65,6 +64,10 @@ def delete_files(local_path):
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
+
+            if os.path.exists(local_path):
+                if len(os.listdir(local_path)) == 0:
+                    os.rmdir(local_path)
 
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
@@ -101,24 +104,31 @@ def separate_csv(filepath):
     file = open(filepath)
     keywords = ['End of Program-Course Details', 'End Of program details', 'End Of Course details',
                 'End of Course enrolments']
+
     mycsv = []
     keyIndex = 0
     key = keywords[keyIndex]
     for row in csv.reader(file):
+
         if row[0] == key:
             if row[0] == 'End of Program-Course Details':
                 df = pd.DataFrame(mycsv)
                 new_header = df.iloc[0].str.strip().str.lower()
+                nan_value = float("NaN")
+                df.replace("", nan_value, inplace=True)
+                df.dropna(how= 'all', inplace=True)
+                df.replace(nan_value,'',inplace= True)
                 df = df[1:]
                 df.columns = new_header
                 df.insert(0, 'program_id', range(1, 1 + len(df)))
-
                 d = pd.melt(df, id_vars=['program_id', 'program_name'], var_name='Course_Id') \
                     .sort_values(['program_id', 'program_name', ]).reset_index(drop=True)
                 df1 = d.drop(columns=['Course_Id']).rename(columns={'value': 'Course_ID'})
+                df1.replace("", nan_value, inplace=True)
                 df2 = df1.dropna(subset=['Course_ID'], inplace=False)
                 col = df2.columns.tolist()
                 val = df2.values.tolist()
+
                 if df2[2:].empty:
                     del df2
                 else:
@@ -128,12 +138,16 @@ def separate_csv(filepath):
             else:
                 df_1 = pd.DataFrame(mycsv)
                 new_header = df_1.iloc[0].str.strip().str.lower()
+                nan_value = float("NaN")
+                df_1.replace("", nan_value, inplace=True)
+                df_1.dropna(how='all', inplace=True)
+                df_1.replace(nan_value, "", inplace=True)
+                df_1.dropna(how='all', axis=1, inplace=True)
                 df_1 = df_1[1:]
                 df_1.columns = new_header
-
                 col = df_1.columns.tolist()
                 val = df_1.values.tolist()
-                if df_1[2:].empty:
+                if df_1.empty:
                     del df_1
                 else:
                     mycsv = [col] + val
@@ -150,16 +164,19 @@ def separate_csv(filepath):
 
 
 if len(sys.argv[1:]) > 0:
+    global local_path
+    local_path = 'new_diksha_directory/'
+    if not os.path.exists(local_path):
+        os.mkdir(local_path)
     path = sys.argv[1]
     storage_type = sys.argv[2]
     emission_dir_path = sys.argv[3]
     separate_csv(path)
     value = count_number(local_path)
+    print(value)
     emission_folder(local_path,emission_dir_path,storage_type)
     update_parameter_ctx("static_data_parameters", "diksha_enrolment_file_count", value)
     delete_files(local_path)
 
 else:
     print('please provide the arguement')
-
-
