7f7c4f42c9c866838d437d4a57fc482274425cf9
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2022-01-28 15:58:16 +0530

    added new common compnent and removed water mark

---
+++ development/ui/dashboard_ui/education_usecase/client-side/src/app/dashboards/group-dashboards/common-dashboard/common-dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommonDashboardComponent } from './common-dashboard.component';
+
+describe('CommonDashboardComponent', () => {
+  let component: CommonDashboardComponent;
+  let fixture: ComponentFixture<CommonDashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CommonDashboardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommonDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
