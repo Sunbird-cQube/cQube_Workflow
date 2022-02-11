ed23175ee9cd89ad7a1f5fa8f029ea40e6951f94
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2022-02-11 11:13:33 +0530

    removed unwanted file

--- development/node_api/dashboard_api/server-side/src/api/controller/users/keycloak-otp/config.js
+++
@@ -1,9 +0,0 @@
-const keyCloakDetails = {
-    "KEYCLOAK_HOST": "http://172.31.28.216:8080",
-    "KEYCLOAK_REALM": "cQube",
-    "KEYCLOAK_CLIENT": "cqube_app",
-    "KEYCLOAK_ADM_USER": "admin",
-    "KEYCLOAK_ADM_PASSWD": "Tibil@123"
-}
-
-module.exports = keyCloakDetails
