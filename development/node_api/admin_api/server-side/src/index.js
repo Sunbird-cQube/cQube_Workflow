38a4081b3a3475c52493a0afcfbaa16b0ce8254c
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2022-02-23 19:37:51 +0530

    fixed vulnerability

--- development/node_api/admin_api/server-side/src/index.js
+++ development/node_api/admin_api/server-side/src/index.js
@@ -16,10 +16,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(compression());
 
+app.use(function (req, res, next) {
+    res.setHeader("Content-Security-Policy", "frame-ancestors 'self';");
+    next();
+});
+
 const router = require('./api/router');
 app.use('/api', router);
 const sessionRouter = require('./api/sessionRouter');
 app.use('/admin_api', sessionRouter);
+
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        console.error(err);
+        return res.status(400).send({ status: 404, message: err.message }); // Bad request
+    }
+    next();
+});
 app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
