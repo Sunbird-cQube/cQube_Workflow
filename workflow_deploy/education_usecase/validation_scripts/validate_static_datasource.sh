b3f560ecdd599eb7bdd76c27e33f9ac9a9ab5d3f
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-04-01 10:33:41 +0530

    modified code for validate_static_datasource.sh

--- workflow_deploy/education_usecase/validation_scripts/validate_static_datasource.sh
+++ workflow_deploy/education_usecase/validation_scripts/validate_static_datasource.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+#set -eu
 config_file=$1
 base_directory=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' $config_file)
 if [[ -e "$base_directory/cqube/.cqube_config" ]]; then
-static_datasource=$(cat $base_directory/cqube/.cqube_config | grep CQUBE_STATIC_DATASOURCE )
-current_datasource=$(cut -d "=" -f2 <<< "$static_datasource")
-datasource_config=$(awk ''/^static_datasource:' /{ if ($2 !~ /#.*/) {print $2}}' $config_file)
-if [[ ! $current_datasource == $datasource_config ]]; then
-    sed -i '/datasource_status/c\datasource_status: unmatched' ../ansible/roles/createdb/vars/main.yml
-else
-    sed -i '/datasource_status/c\datasource_status: matched' ../ansible/roles/createdb/vars/main.yml
+    static_datasource=$( grep CQUBE_STATIC_DATASOURCE $base_directory/cqube/.cqube_config )
+    current_datasource=$(cut -d "=" -f2 <<< "$static_datasource")
+    if [[ ! $current_datasource == "" ]]; then
+        datasource_config=$(awk ''/^static_datasource:' /{ if ($2 !~ /#.*/) {print $2}}' $config_file)
+        if [[ ! $current_datasource == $datasource_config ]]; then
+            sed -i '/datasource_status/c\datasource_status: unmatched' ../ansible/roles/createdb/vars/main.yml
+            echo "static_datasource value from config.yml is not matching with previous installation value. If continued with this option, it will truncate the tables from db and clear the objects from output bucket."
+            while true; do
+                read -p "yes to continue, no to cancel the installation (yes/no)? : " yn
+                case $yn in
+                    yes) break;;
+                    no) echo "Cancelling the installation." 
+                        exit ;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+            done
+            echo "continuing"
+        else
+            sed -i '/datasource_status/c\datasource_status: matched' ../ansible/roles/createdb/vars/main.yml
+        fi
+    fi
 fi
-fi
+
