6d3273acc6062bd9296aa3711bec6d132501993c
ayesha-siddiq123 <ayesha.siddiq@tibilsolutions.com>
2022-04-25 16:46:00 +0530

    hosts inventory file added

--- workflow_deploy/education_usecase/install_ui.sh
+++ workflow_deploy/education_usecase/install_ui.sh
@@ -3,7 +3,7 @@
 base_dir=$(awk ''/^base_dir:' /{ if ($2 !~ /#.*/) {print $2}}' config.yml)
 mode_of_installation=$(awk ''/^mode_of_installation:' /{ if ($2 !~ /#.*/) {print $2}}' $base_dir/cqube/conf/base_config.yml)
 if [[ $mode_of_installation == "localhost" ]]; then
-ansible-playbook ../ansible/install_ui.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
+ansible-playbook -i hosts ../ansible/install_ui.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
                                                              --extra-vars "@config.yml" \
 							                                 --extra-vars "@memory_config.yml" \
                                                              --extra-vars "@.version" \
@@ -14,7 +14,7 @@ ansible-playbook ../ansible/install_ui.yml --tags "install" --extra-vars "@$base
                                                              --extra-vars "usecase_name=education_usecase" \
                                                              --extra-vars "protocol=http"
 else
-ansible-playbook ../ansible/install_ui.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
+ansible-playbook -i hosts ../ansible/install_ui.yml --tags "install" --extra-vars "@$base_dir/cqube/conf/base_config.yml" \
                                                              --extra-vars "@config.yml" \
 							                                 --extra-vars "@memory_config.yml" \
                                                              --extra-vars "@.version" \
