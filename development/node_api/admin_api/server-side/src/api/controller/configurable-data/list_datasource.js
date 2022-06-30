48a256ce09c9484b49c2bb7def1c507b7b48ccd7
Sharathnaik38 <sharath.naik@tibilsolutions.com>
2022-06-30 16:10:13 +0530

    listing .sql files and passing parameter to nifi functionality is added

---
+++ development/node_api/admin_api/server-side/src/api/controller/configurable-data/list_datasource.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { logger } = require('../../lib/logger');
+const auth = require('../../middleware/check-auth');
+const fs = require('fs');
+const csv = require('csvtojson')
+const path = require('node:path');
+const shell = require('shelljs');
+const baseDir = process.env.BASE_DIR;
+const { PythonShell } = require('python-shell');
+router.get('/', async function (req, res) {
+    try {
+        logger.info('---list new dataSource api ---');
+        let csvFilePath = `${process.env.BASE_DIR}/cqube/emission_app/python/postgres`;
+        let jsonArray = [];
+        // read directory
+        fs.readdir(csvFilePath, (error, fileNames) => {
+            if (error) throw error;
+            fileNames.forEach(filename => {
+                const ext = path.parse(filename).ext;
+                if (ext === ".sql") {
+                    let name = filename.substr(0, filename.indexOf('.'))
+                    jsonArray.push(name)
+
+                }
+            }
+            );
+            res.status(200).send(jsonArray);
+        });
+
+    } catch (e) {
+        logger.error(`Error :: ${e}`)
+        res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+    }
+});
+
+router.post('/buildUI', async function (req, res) {
+    try {
+        let dataSource = req.body.dataSource
+        console.log('req', req.body)
+        logger.info('---buildUI ---');
+        let UIpath = `${process.env.ANGULAR_DIRECTORY}`;
+        shell.echo('--shell script started----')
+        var output = shell.exec(`fuser -n tcp -k 4200`, function (stdout, stderr, code){
+            var output1 = shell.exec(`cd  ${UIpath} && ng serve`, function (stdout, stderr, code){            
+         })
+        })
+        var pyth1 = shell.exec(`sudo ${process.env.BASE_DIR}/cqube/emission_app/flaskenv/bin/python ${baseDir}/cqube/emission_app/python/configurable.py ${dataSource} `, function (stdout, stderr, code) {
+            console.log('code', code)
+        })
+    } catch (e) {
+        logger.error(`Error :: ${e}`)
+        res.status(500).json({ errMessage: "Internal error. Please try again!!" });
+    }
+});
+
+
+module.exports = router;
